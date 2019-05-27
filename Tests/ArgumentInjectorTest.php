e58807b5fcdd7527fe73f99ebcbf423bb580bff9
Thomas Abplanalp <info@tasoft.ch>
2019-05-27 17:51:08 +0200

    ADD: Function arguments injector

---
+++ Tests/ArgumentInjectorTest.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Copyright (c) 2019 TASoft Applications, Th. Abplanalp <info@tasoft.ch>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * ArgumentInjectorTest.php
+ * php-dependency-injection
+ *
+ * Created on 2019-05-27 17:16 by thomas
+ */
+
+use PHPUnit\Framework\Error\Warning;
+use PHPUnit\Framework\TestCase;
+use TASoft\DI\Injector\FunctionArgumentInjector;
+
+class ArgumentInjectorTest extends TestCase
+{
+    public function testScalarArgumentInjector() {
+        $ai = new FunctionArgumentInjector(
+            1, "Test", 0.5, true, NULL
+        );
+
+        $this->assertEquals([
+            'INT' => 1,
+            "STRING" => "Test",
+            "FLOAT" => 0.5,
+            "BOOLEAN" => true,
+            "NULL" => NULL
+        ], $ai->getArguments());
+    }
+
+    /**
+     * @expectedException PHPUnit\Framework\Error\Warning
+     */
+    public function testDuplicatedArguments() {
+        $ai = new FunctionArgumentInjector(
+            1, 2, 3
+        );
+    }
+
+    public function testNamedArguments() {
+        $ai = new FunctionArgumentInjector([
+            'test' => 23,
+            'String',
+            'flag' => false
+        ]);
+
+        $this->assertEquals([
+            "INT" => 23,
+            "STRING" => "String",
+            "BOOLEAN" => false
+        ], $ai->getArguments());
+
+        $this->assertEquals([
+            'test' => 23,
+            'flag' => false
+        ], $ai->getNames());
+    }
+
+    public function testObjectArguments() {
+        $ai = new FunctionArgumentInjector(
+            $o1 = new stdClass,
+            $o2 = new FunctionArgumentInjector()
+        );
+
+        $this->assertEquals([
+            'STDCLASS' => $o1,
+            'OBJECT' => $o1,
+            'TASOFT\DI\INJECTOR\FUNCTIONARGUMENTINJECTOR' => $o2
+        ], $ai->getArguments());
+
+        $this->assertEquals($o1, $ai->getDependency('object', NULL));
+        $this->assertEquals($o1, $ai->getDependency('object', 'unknown name'));
+
+        $this->assertEquals($o2, $ai->getDependency(FunctionArgumentInjector::class, "hehe"));
+        $this->assertEquals($o2, $ai->getDependency(FunctionArgumentInjector::class, NULL));
+
+        $this->assertNull($ai->getDependency(NULL, NULL));
+        $this->assertNull($ai->getDependency(ArgumentInjectorTest::class, NULL));
+    }
+}
---
+++ src/Injector/FunctionArgumentInjector.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Copyright (c) 2019 TASoft Applications, Th. Abplanalp <info@tasoft.ch>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace TASoft\DI\Injector;
+
+
+use Traversable;
+
+class FunctionArgumentInjector implements InjectorInterface
+{
+    /** @var array */
+    private $arguments = [];
+    private $namedArguments = [];
+
+    /**
+     * FunctionArgumentInjector constructor.
+     * @param iterable|mixed[] $arguments   Accepts an iterable or arguments
+     */
+    public function __construct(...$arguments)
+    {
+        $addType = function($type) use (&$argument) {
+            $type = strtoupper($type);
+
+            if(isset($this->arguments[$type])) {
+                if($type != 'OBJECT')
+                    trigger_error("Argument of type $type already exists", E_USER_WARNING);
+                return;
+            }
+
+            $this->arguments[$type] = $argument;
+        };
+
+        // If passed one iterable, treat it as arguments
+        if(count($arguments) == 1 && is_array($arguments[0]))
+            $arguments = $arguments[0];
+
+        foreach($arguments as $key => $argument) {
+            $type = gettype($argument);
+
+            if($type == 'integer')
+                $type = 'int';
+            if($type == 'double')
+                $type = 'float';
+            if($type == 'bool')
+                $type = 'boolean';
+
+            if($type == 'object') {
+                $class = get_class($argument);
+                do {
+                    $addType($class);
+                    $class = get_parent_class($class);
+                } while ($class);
+            }
+
+            if(is_string($key))
+                $this->namedArguments[$key] = $argument;
+
+            $addType($type);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDependency(?string $type, ?string $name)
+    {
+        if($name && isset($this->namedArguments[$name]))
+            return $this->namedArguments[$name];
+
+        do {
+            if(isset($this->arguments[ strtoupper($type) ])) {
+                return $this->arguments[ strtoupper($type) ];
+            }
+            $type = get_parent_class($type);
+        } while($type);
+        return NULL;
+    }
+
+    /**
+     * @return array
+     */
+    public function getArguments(): array
+    {
+        return $this->arguments;
+    }
+
+    /**
+     * @return array
+     */
+    public function getNames(): array {
+        return $this->namedArguments;
+    }
+}
